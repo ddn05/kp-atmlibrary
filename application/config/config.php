f43097e4ebfee38921c0c456252d1ebfdbdcdead
dadan05 <dadannazril@gmail.com>
2020-09-23 17:54:26 +0700

    menambahkan icon sidebar

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/kp-atmlibrary/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
