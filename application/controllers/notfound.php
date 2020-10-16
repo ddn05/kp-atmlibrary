60dfa4cd1f68402d537a0c7cb5b19098d176fc7b
ddn05 <dadannazril@gmail.com>
2020-10-16 17:34:25 +0700

    menambahkan fitur 404 not found

---
+++ application/controllers/notfound.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Notfound extends CI_Controller {
+    public function index(){
+        $data['title'] = "NOT FOUND";
+
+        $this->load->view('template/header',$data);
+        $this->load->view('404');
+        $this->load->view('template/footer');
+    }
+}
---
+++ application/views/404.php
@@ -0,0 +1,12 @@
+<div class="container-fluid">
+
+        <!-- 404 Error Text -->
+        <div class="text-center mt-5">
+            <div class="error mx-auto" data-text="404">404</div>
+            <p class="lead text-gray-800 mb-5">Henteu aya halaman</p>
+            <p class="text-gray-500 mb-1">mending uih deui</p>
+            <a href="<?php echo base_url()?>">&larr; Uih</a>
+        </div>
+
+        </div>
+<!-- /.container-fluid -->
